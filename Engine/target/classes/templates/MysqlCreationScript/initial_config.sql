8b93b824c1c610f4230c30b1d73ae5fb7209ab95
inad9300 <inad9300@gmail.com>
2016-03-27 20:35:13 +0200

    Adding Maven's target directory to .gitignore file

--- Engine/target/classes/templates/MysqlCreationScript/initial_config.sql
+++
@@ -1,8 +0,0 @@
-SET NAMES {{charset}} COLLATE utf8_unicode_ci;
-
-CREATE DATABASE IF NOT EXISTS `{{dbName}}`
-	DEFAULT CHARACTER SET {{charset}}
-	DEFAULT COLLATE utf8_unicode_ci;
-
-USE `{{dbName}}`;
-
